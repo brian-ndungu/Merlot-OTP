35882b5994e88aa21647e1f710911a8f243d3471
Tlou Lebelo <u15209190@tuks.co.za>
2019-03-13 22:10:06 +0200

    Update src/OneTimePin.java
    Added code skeleton

---
+++ src/OneTimePin.java
@@ -0,0 +1,22 @@
+/** 
+ * Provides one time pin function to FNB app
+*/
+public class OneTimePin{
+    
+    void OneTimePin(){
+
+    }
+    /** Communicate with Notification */
+    private void sendOTP(){
+
+    }
+
+    /** Communicate with ATM */
+    private void verifyOTP(){
+
+    }
+
+    public String getAuthenticationStatus(){
+
+    }
+}
