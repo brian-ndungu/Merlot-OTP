11beb907085f58c46f06bb535b812941f27befcf
brian-ndungu <u15322913@tuks.co.za>
2019-03-28 00:01:03 +0200

    RESTfull API Version 0.2

--- otpservice/src/main/java/com/fnbatm/otpservice/OTPResource.java
+++ otpservice/src/main/java/com/fnbatm/otpservice/OTPResource.java
@@ -14,13 +14,13 @@ public class OTPResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getOTPStatus(@PathParam("clientid") String clientid) throws InterruptedException {
 		//CASE 1: Ideal Situation
-//		OTPClient tempClient= new OTPClient(clientid);
-//		OTP curOTP= new OTP(tempClient);
-//		//OTP is generated
-//		curOTP.setOTP("256");
-//		//OTP is typed in
-//		curOTP.setATMOTP("256");
-//		tempClient= curOTP.verifyOTP(tempClient, true);
+		OTPClient tempClient= new OTPClient(clientid);
+		OTP curOTP= new OTP(tempClient);
+		//OTP is generated
+		curOTP.setOTP("256");
+		//OTP is typed in
+		curOTP.setATMOTP("256");
+		tempClient= curOTP.verifyOTP(tempClient, true, true);
 		
 		//CASE 2: Incorrect OTP
 //		OTPClient tempClient= new OTPClient(clientid);
@@ -41,13 +41,13 @@ public class OTPResource {
 //		tempClient= curOTP.verifyOTP(tempClient, false, true);
 		
 		//CASE 4: ATMSimulation Exception
-		OTPClient tempClient= new OTPClient(clientid);
-		OTP curOTP= new OTP(tempClient);
-		//OTP is generated
-		curOTP.generateOTP(5);
-		//OTP is generated
-		curOTP.setATMOTP("256");
-		tempClient= curOTP.verifyOTP(tempClient, true, false);
+//		OTPClient tempClient= new OTPClient(clientid);
+//		OTP curOTP= new OTP(tempClient);
+//		//OTP is generated
+//		curOTP.generateOTP(5);
+//		//OTP is generated
+//		curOTP.setATMOTP("256");
+//		tempClient= curOTP.verifyOTP(tempClient, true, false);
 		
 		
 		return tempClient.statusMsg;
